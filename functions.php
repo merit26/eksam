5cde2267882e9c69a7bc75db55add2074cf79add
Merit Paist <meripai@greeny.cs.tlu.ee>
2016-01-12 12:02:45 +0200

    create user

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+	require_once("../../config_global.php");
+    require_once("User.class.php");
+	$database = "if15_merit26_2";
+	
+	session_start();
+	
+	$mysqli = new mysqli($servername, $server_username, $server_password, $database);
+	$User = new User($mysqli);
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once("../header.php"); ?>
+
+
+<div class="span12">
+    <div style="padding:0 35px">
+      <div class="row-fluid">
+...
+Ajakirjaniku võimalus leida endale vaheldusrikas töö!
+<br>
+Ettevõtte võimalus leida oma firmat esindavale tekstile hea tegija - küsi pakkumisi, võrdle ja vali parim!
+
+</div>
+</div>
+</div>
+<?php require_once("../footer.php"); ?>
